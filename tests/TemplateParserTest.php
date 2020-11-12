4294811805f160f79bd1329e6c336ec44ca8f459
jcheron <myaddressmail@gmail.com>
2020-11-13 00:47:37 +0100

    add codception

---
+++ tests/TemplateParserTest.php
@@ -0,0 +1,69 @@
+<?php
+use PHPMV\core\TemplateParser;
+
+/**
+ * TemplateParser test case.
+ */
+class TemplateParserTest extends \Codeception\Test\Unit {
+
+	/**
+	 *
+	 * @var TemplateParser
+	 */
+	private $templateParser;
+
+	protected function _before() {}
+
+	protected function _after() {}
+
+	/**
+	 * Tests TemplateParser->loadTemplatefile()
+	 */
+	public function testLoadTemplatefile() {
+		// TODO Auto-generated TemplateParserTest->testLoadTemplatefile()
+		$this->markTestIncomplete("loadTemplatefile test not implemented");
+
+		$this->templateParser->loadTemplatefile(/* parameters */);
+	}
+
+	/**
+	 * Tests TemplateParser->parse()
+	 */
+	public function testParse() {
+		// TODO Auto-generated TemplateParserTest->testParse()
+		$this->markTestIncomplete("parse test not implemented");
+
+		$this->templateParser->parse(/* parameters */);
+	}
+
+	/**
+	 * Tests TemplateParser->loadAndParse()
+	 */
+	public function testLoadAndParse() {
+		// TODO Auto-generated TemplateParserTest->testLoadAndParse()
+		$this->markTestIncomplete("loadAndParse test not implemented");
+
+		$this->templateParser->loadAndParse(/* parameters */);
+	}
+
+	/**
+	 * Tests TemplateParser::getTemplateDirectory()
+	 */
+	public function testGetTemplateDirectory() {
+		// TODO Auto-generated TemplateParserTest::testGetTemplateDirectory()
+		$this->markTestIncomplete("getTemplateDirectory test not implemented");
+
+		TemplateParser::getTemplateDirectory(/* parameters */);
+	}
+
+	/**
+	 * Tests TemplateParser::setTemplateDirectory()
+	 */
+	public function testSetTemplateDirectory() {
+		// TODO Auto-generated TemplateParserTest::testSetTemplateDirectory()
+		$this->markTestIncomplete("setTemplateDirectory test not implemented");
+
+		TemplateParser::setTemplateDirectory(/* parameters */);
+	}
+}
+
