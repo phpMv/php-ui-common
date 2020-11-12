696ee7acb5640af3b87ec3878e1dff97ff7b99c2
jcheron <myaddressmail@gmail.com>
2020-11-13 00:00:10 +0100

    add composer.json

---
+++ src/PHPMV/core/TemplateParser.php
@@ -0,0 +1,16 @@
+<?php
+namespace PHPMV\core;
+
+/**
+ * PHPMV\core$TemplateParser
+ * This class is part of php-ui-common
+ *
+ * @author jc
+ * @version 1.0.0
+ *
+ */
+class TemplateParser {
+
+	public function loadTemplatefile(string $filename): void {}
+}
+
