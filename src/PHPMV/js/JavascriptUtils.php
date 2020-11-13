c3aa2d538dd197b842aef01e75b15112b4231673
jcheron <myaddressmail@gmail.com>
2020-11-13 23:49:52 +0100

    JsUtils->JavascriptUtils

--- src/PHPMV/js/JavascriptUtils.php
+++ src/PHPMV/js/JavascriptUtils.php
@@ -3,14 +3,14 @@ namespace PHPMV\js;
 
 /**
  * Javascript utilities.
- * PHPMV\js$JsUtils
+ * PHPMV\js$JavascriptUtils
  * This class is part of php-ui-common
  *
  * @author jc
  * @version 1.0.0
  *
  */
-class JsUtils {
+class JavascriptUtils {
 
 	/**
 	 * Returns a JSON string from an object.
@@ -18,7 +18,7 @@ class JsUtils {
 	 * @param mixed $object
 	 * @return string
 	 */
-	public static function objectToJSON($object): string {
+	public static function toJSON($object): string {
 		if (\is_object($object)) {
 			if (\method_exists($object, 'toArray')) {
 				$object = $object->toArray();
@@ -37,9 +37,9 @@ class JsUtils {
 	 */
 	public static function wrapScript(string $script): string {
 		if ($script == null) {
-			return "";
+			return '';
 		}
-		if (\substr($script, 0, strlen("<script>")) !== "<script>") {
+		if (\substr($script, 0, \strlen('<script>')) !== '<script>') {
 			$script = "<script>$script</script>";
 		}
 		return $script;
